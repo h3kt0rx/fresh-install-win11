0d6b76c689ad4109c70e1af975a231ace177240b
h3kt0rx <57811889+h3kt0rx@users.noreply.github.com>
2024-06-08 23:58:57 -0400

    Update OOSU.ps1

--- OOSU.ps1
+++ OOSU.ps1
@@ -4,6 +4,6 @@ $OOSU_filepath = "$ENV:temp\OOSU10.exe"
     $ProgressPreference = "SilentlyContinue" # Disables the Progress Bar to drasticly speed up Invoke-WebRequest
     Invoke-WebRequest -Uri "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile $OOSU_filepath
     $oosu_config = "$ENV:temp\ooshutup10.cfg"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/config/ooshutup10.cfg" -OutFile $oosu_config
+    Invoke-WebRequest -Uri "https://github.com/h3kt0rx/fresh-install-win11/blob/main/ooshutup10.cfg" -OutFile $oosu_config
     Write-Host "Applying recommended OO Shutup 10 Policies"
     Start-Process $OOSU_filepath -ArgumentList "$oosu_config /quiet" -Wait
