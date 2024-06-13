2f7e66d30fba56bd592f4983529ee69df945c9a4
John Doe <johndoe@example.com>
2024-06-13 19:22:56 -0400

    Update

--- Windows-Settings.ps1
+++ Windows-Settings.ps1
@@ -14,7 +14,7 @@ $OOSU_filepath = "$ENV:temp\OOSU10.exe"
 $oosu_config = "$ENV:temp\ooshutup10.cfg"
 Invoke-WebRequest -Uri "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile $OOSU_filepath
 Invoke-WebRequest -Uri "https://github.com/h3kt0rx/fresh-install-win11/blob/main/cfg/ooshutup10.cfg" -OutFile $oosu_config
-Write-Host "Applying recommended OO Shutup 10 Policies"
+Write-Host "Applying personal O&O Shutup 10 Policies"
 Start-Process $OOSU_filepath -ArgumentList "$oosu_config /quiet" -Wait
 Clear-Host
 ############################################################################################################################################################
