3b11a471e3cbcb4caad017902ec4208b650e0745
h3kt0rx <h3kt0r.x@gmail.com>
2024-09-27 04:19:25 -0400

    tst

--- test-file.ps1
+++ test-file.ps1
@@ -1,5 +1,5 @@
 # Define the URL for the DirectX installer
-$directxUrl = "https://download.microsoft.com/download/1/8/3/183E4F90-9F52-4E85-B733-FD13F9B0CE5F/directx_websetup.exe"
+$directxUrl = "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe"
 
 # Define the path for the downloaded installer
 $installerPath = "$env:TEMP\directx_installer.exe"
