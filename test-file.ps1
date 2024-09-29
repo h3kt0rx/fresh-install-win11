1a3de0dcd64de13f1c3137d59bfedbb8744c1457
h3kt0rx <h3kt0r.x@gmail.com>
2024-09-29 11:40:22 -0400

    dxtest

--- test-file.ps1
+++ test-file.ps1
@@ -1,5 +1,5 @@
 $FileUri = "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe"
-$Destination = $env:TEMP
+$Destination = $env:TEMP/directx_Jun2010_redist.exe
 
 $bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
 
