191d4e01e0d12f301086ef2c0e734239366d213e
h3kt0rx <h3kt0r.x@gmail.com>
2024-09-30 03:00:24 -0400

    tst

--- test-file.ps1
+++ test-file.ps1
@@ -1,23 +1,19 @@
-# Define the file URI and paths
 $FileUri = "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe"
-$Destination = "$env:TEMP\directx_Jun2010_redist.exe"
-$runFile = $Destination
+$Destination = "$env:TEMP/directx_Jun2010_redist.exe"
 
-# Download the file using BITS
-$bitsJobObj = Start-BitsTransfer -Source $FileUri -Destination $Destination
+$bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
 
 switch ($bitsJobObj.JobState) {
+
     'Transferred' {
         Complete-BitsTransfer -BitsJob $bitsJobObj
         break
     }
+
     'Error' {
         throw 'Error downloading'
     }
 }
 
-# Start the installer with silent mode
-Start-Process -FilePath $runFile -ArgumentList "/silent" -Wait
 
-# Clean up
-Remove-Item -Path $Destination -Force
+Start-Process -Wait $Destination -ArgumentList "/Q"
