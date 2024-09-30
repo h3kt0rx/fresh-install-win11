33911d5e590b8c90cf7db6cf72bf8fbaf41b2dc0
h3kt0rx <h3kt0r.x@gmail.com>
2024-09-30 02:55:37 -0400

    tst

--- test-file.ps1
+++ test-file.ps1
@@ -1,6 +1,7 @@
 $FileUri = "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe"
 $Destination = "$env:TEMP/directx_Jun2010_redist.exe"
-
+$destinationPath = "$env:TEMP/directx_Jun2010_redist"
+$runFile = "$env:TEMP/$destinationPath/DXSETUP.exe"
 $bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
 
 switch ($bitsJobObj.JobState) {
@@ -15,5 +16,5 @@ switch ($bitsJobObj.JobState) {
     }
 }
 
-
-Start-Process -Wait $Destination -ArgumentList "/Q"
+Expand-Archive -Path $Destination -DestinationPath $destinationPath
+Start-Process -Wait $runFile -ArgumentList "/silent"
