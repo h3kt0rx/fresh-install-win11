16a3552fe3f3e6d8942c1245ff5ade6d3b1a1c19
h3kt0rx <h3kt0r.x@gmail.com>
2024-09-30 03:12:23 -0400

    tst

--- test-file.ps1
+++ test-file.ps1
@@ -20,12 +20,14 @@ switch ($bitsJobObj.JobState) {
 if (-Not (Test-Path -Path $ExtractPath)) {
     New-Item -ItemType Directory -Path $ExtractPath | Out-Null
 }
-
+Get-FileFromWeb -URL "https://www.7-zip.org/a/7z2301-x64.exe" -File "$env:TEMP\7-Zip.exe"
 # Run the installer to extract files to the specified directory
-Start-Process -FilePath $Destination -ArgumentList "/C /T:$ExtractPath" -Wait
-
-# Optional: Check the extracted files
-Get-ChildItem -Path $ExtractPath
+Start-Process -FilePath $Destination -ArgumentList "$ExtractPath" -Wait
+Start-Process -wait "$env:TEMP\7-Zip.exe" /S
+# extract files with 7zip
+cmd /c "C:\Program Files\7-Zip\7z.exe" x "$Destination" -o"$ExtractPath" -y | Out-Null
+# install direct x
+Start-Process "$ExtractPath\DXSETUP.exe" -ArgumentList "/silent"
 
 # Clean up the installer
 Remove-Item -Path $Destination -Force
