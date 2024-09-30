6c22338e5124d357eae990e06642fb1ecc3266e6
h3kt0rx <h3kt0r.x@gmail.com>
2024-09-30 03:14:56 -0400

    tst

--- test-file.ps1
+++ test-file.ps1
@@ -1,10 +1,13 @@
 # Define the URL and paths
 $FileUri = "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/dxwebsetup.exe"
+$FileUri2 = "https://www.7-zip.org/a/7z2301-x64.exe"
 $Destination = "$env:TEMP\dxwebsetup.exe"
+$Destination2 = "$env:TEMP\7-Zip.exe"
 $ExtractPath = "$env:TEMP\DirectX_Install"
 
 # Download the DirectX Web Setup
 $bitsJobObj = Start-BitsTransfer -Source $FileUri -Destination $Destination
+$bitsJobObj = Start-BitsTransfer -Source $FileUri2 -Destination $Destination2
 
 switch ($bitsJobObj.JobState) {
     'Transferred' {
@@ -20,9 +23,7 @@ switch ($bitsJobObj.JobState) {
 if (-Not (Test-Path -Path $ExtractPath)) {
     New-Item -ItemType Directory -Path $ExtractPath | Out-Null
 }
-Get-FileFromWeb -URL "https://www.7-zip.org/a/7z2301-x64.exe" -File "$env:TEMP\7-Zip.exe"
-# Run the installer to extract files to the specified directory
-Start-Process -FilePath $Destination -ArgumentList "$ExtractPath" -Wait
+
 Start-Process -wait "$env:TEMP\7-Zip.exe" /S
 # extract files with 7zip
 cmd /c "C:\Program Files\7-Zip\7z.exe" x "$Destination" -o"$ExtractPath" -y | Out-Null
