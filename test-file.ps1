489f5a10924a5110a3e1a8fc5c4edd514795525e
h3kt0rx <h3kt0r.x@gmail.com>
2024-09-30 03:50:33 -0400

    tst

--- test-file.ps1
+++ test-file.ps1
@@ -1,17 +1,17 @@
 # Define the URL and paths
-$FileUri = "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe"
-$FileUri2 = "https://www.7-zip.org/a/7z2301-x64.exe"
-$Destination = "$env:TEMP\directx_Jun2010_redist.exe"
-$Destination2 = "$env:TEMP\7-Zip.exe"
-$ExtractPath = "$env:TEMP\DirectX_Install"
+$DXFileUri = "https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe"
+$DXFileUri2 = "https://www.7-zip.org/a/7z2301-x64.exe"
+$DXDestination = "$env:TEMP\directx_Jun2010_redist.exe"
+$DXDestination2 = "$env:TEMP\7-Zip.exe"
+$DXExtractPath = "$env:TEMP\DirectX_Install"
 
 # Download the DirectX Web Setup
-$bitsJobObj = Start-BitsTransfer -Source $FileUri -Destination $Destination
-$bitsJobObj = Start-BitsTransfer -Source $FileUri2 -Destination $Destination2
+$DXbitsJobObj = Start-BitsTransfer -Source $DXFileUri -Destination $DXDestination
+$DXbitsJobObj = Start-BitsTransfer -Source $DXFileUri2 -Destination $DXDestination2
 
-switch ($bitsJobObj.JobState) {
+switch ($DXbitsJobObj.JobState) {
     'Transferred' {
-        Complete-BitsTransfer -BitsJob $bitsJobObj
+        Complete-BitsTransfer -BitsJob $DXbitsJobObj
         break
     }
     'Error' {
@@ -20,15 +20,16 @@ switch ($bitsJobObj.JobState) {
 }
 
 # Create the extraction directory if it doesn't exist
-if (-Not (Test-Path -Path $ExtractPath)) {
-    New-Item -ItemType Directory -Path $ExtractPath | Out-Null
+if (-Not (Test-Path -Path $DXExtractPath)) {
+    New-Item -ItemType Directory -Path $DXExtractPath | Out-Null
 }
 
 Start-Process -wait "$env:TEMP\7-Zip.exe" /S
 # extract files with 7zip
-cmd /c "C:\Program Files\7-Zip\7z.exe" x "$Destination" -o"$ExtractPath" -y | Out-Null
+cmd /c "C:\Program Files\7-Zip\7z.exe" x "$DXDestination" -o"$DXExtractPath" -y | Out-Null
 # install direct x
-Start-Process "$ExtractPath\DXSETUP.exe" -ArgumentList "/silent" -Wait
+Start-Process "$DXExtractPath\DXSETUP.exe" -ArgumentList "/silent" -Wait
 
 # Clean up the installer
-Remove-Item -Path $Destination -Force
+Remove-Item -Path $DXDestination -Force
+Write-Output "Updated DirectX"
