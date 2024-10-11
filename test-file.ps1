89c9de1dd4ac25dbed959c07c0c0bbed28656f0a
h3kt0rx <h3kt0r.x@gmail.com>
2024-10-11 14:12:43 -0400

    ed

--- test-file.ps1
+++ test-file.ps1
@@ -9,7 +9,6 @@ function Enable-GameMode {
     # Set or create properties
     New-ItemProperty -Path $gameModePath -Name "AutoGameModeEnabled" -Value 1 -PropertyType DWord -Force
     New-ItemProperty -Path $gameModePath -Name "UseGameMode" -Value 1 -PropertyType DWord -Force
-    
     Write-Host "Game Mode has been enabled."
 }
 
