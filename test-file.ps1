c601ca212ac44c48ae0df4025d9d476c63b5e3af
h3kt0rx <h3kt0r.x@gmail.com>
2024-10-11 14:11:40 -0400

    test

--- test-file.ps1
+++ test-file.ps1
@@ -13,5 +13,19 @@ function Enable-GameMode {
     Write-Host "Game Mode has been enabled."
 }
 
-# Run the function with administrative privileges
+# Function to Disable Core Isolation Memory Integrity
+function Disable-CoreIsolation {
+    $memoryIntegrityPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceGuard"
+    
+    if (-not (Test-Path $memoryIntegrityPath)) {
+        New-Item -Path $memoryIntegrityPath -Force
+    }
+
+    # Set Memory Integrity to disabled
+    Set-ItemProperty -Path $memoryIntegrityPath -Name "EnableVirtualizationBasedSecurity" -Value 0 -ErrorAction SilentlyContinue
+    Write-Host "Core Isolation Memory Integrity has been disabled."
+}
+
+# Execute Optimization Functions
+Disable-CoreIsolation
 Enable-GameMode
