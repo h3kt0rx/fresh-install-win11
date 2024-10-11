cfaabc9bd327c922dccd53e51d7aa0400bb678d9
h3kt0rx <h3kt0r.x@gmail.com>
2024-10-11 13:50:43 -0400

    test

--- test-file.ps1
+++ test-file.ps1
@@ -1,27 +1,17 @@
-# Function to Enable Game Mode
 function Enable-GameMode {
     $gameModePath = "HKLM:\SOFTWARE\Microsoft\GameBar"
+    
+    # Check if the path exists, create it if it doesn't
     if (-not (Test-Path $gameModePath)) {
         New-Item -Path $gameModePath -Force
     }
-    Set-ItemProperty -Path $gameModePath -Name "AutoGameModeEnabled" -Value 1 -ErrorAction SilentlyContinue
-#    Set-ItemProperty -Path $gameModePath -Name "UseGameMode" -Value 1 -ErrorAction SilentlyContinue
+    
+    # Set or create properties
+    New-ItemProperty -Path $gameModePath -Name "AutoGameModeEnabled" -Value 1 -PropertyType DWord -Force
+    New-ItemProperty -Path $gameModePath -Name "UseGameMode" -Value 1 -PropertyType DWord -Force
+    
     Write-Host "Game Mode has been enabled."
 }
 
-# Function to Disable Core Isolation Memory Integrity
-function Disable-CoreIsolation {
-    $memoryIntegrityPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceGuard"
-    
-    if (-not (Test-Path $memoryIntegrityPath)) {
-        New-Item -Path $memoryIntegrityPath -Force
-    }
-
-    # Set Memory Integrity to disabled
-    Set-ItemProperty -Path $memoryIntegrityPath -Name "EnableVirtualizationBasedSecurity" -Value 0 -ErrorAction SilentlyContinue
-    Write-Host "Core Isolation Memory Integrity has been disabled."
-}
-
-# Execute Optimization Functions
-Disable-CoreIsolation
+# Run the function with administrative privileges
 Enable-GameMode
